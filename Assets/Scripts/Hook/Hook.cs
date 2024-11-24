0a442dc051b8918b3908ed76ea5684d50f459918
harigovind11 <harigovindsreejith7@gmail.com>
2024-11-24 11:53:38 +0530

    Hook X movement

--- Assets/Scripts/Hook/Hook.cs
+++ Assets/Scripts/Hook/Hook.cs
@@ -1,27 +1,42 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+using UnityEngine.Rendering;
 
 public class Hook : MonoBehaviour
 {
     public Transform hookedTransform;
 
-   Camera mainCamera;
+    Camera mainCamera;
     Collider2D coll;
-    
 
-     void Awake()
+    int length;
+    int strength;
+    int fishCount;
+
+    bool canMove = true;
+
+    //List<fish>
+
+    Tweener cameraTween;
+
+
+    void Awake()
     {
-        
-    }
-    void Start()
-    {
-        
+        mainCamera = Camera.main;
+        coll = GetComponent<Collider2D>();
+        //List<fish>
     }
 
     void Update()
     {
-        
+        if (canMove && Input.GetMouseButton(0))
+        {
+            Vector3 vector=mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = transform.position;
+            position.x = vector.x;
+            transform.position = position;
+        }
     }
 }
