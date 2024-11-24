1629b65b0302edb0be20b65bf55ee42cc9f6a7c0
harigovind11 <harigovindsreejith7@gmail.com>
2024-11-24 11:27:15 +0530

    imported assets and created hook script

---
+++ Assets/Scripts/Hook/Hook.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Hook : MonoBehaviour
+{
+    public Transform hookedTransform;
+
+   Camera mainCamera;
+    Collider2D coll;
+    
+
+     void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
