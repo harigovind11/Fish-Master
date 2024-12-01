d334c1ba4d5bda2cc69179ec3c7b5720c4f0c478
harigovind11 <harigovindsreejith7@gmail.com>
2024-12-01 19:17:48 +0530

    UI linking

--- Assets/Scripts/Hook/Hook.cs
+++ Assets/Scripts/Hook/Hook.cs
@@ -1,26 +1,25 @@
-using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
+using DG.Tweening;
 
 public class Hook : MonoBehaviour
 {
- public Transform hookedTransform;
 
-    Camera mainCamera;
-    Collider2D coll;
+    public Transform hookedTransfrom;
 
-    int length;
-    int strength;
-    int fishCount;
+    private Camera mainCamera;
+    private Collider2D coll;
 
-    bool canMove;
+    private int length;
+    private int strength;
+    private int fishCount;
 
-    List<Fish> hookedFishes;
+    private bool canMove;
 
-    Tweener cameraTween;
+    private List<Fish> hookedFishes;
 
+    private Tweener cameraTween;
 
     void Awake()
     {
@@ -42,33 +41,33 @@ public class Hook : MonoBehaviour
 
     public void StartFishing()
     {
-        length = -50;
-        strength = 3;
+        length = IdleManager.instance.length - 20;
+        strength = IdleManager.instance.strength;
         fishCount = 0;
-
         float time = (-length) * 0.1f;
 
         cameraTween = mainCamera.transform.DOMoveY(length, 1 + time * 0.25f, false).OnUpdate(delegate
         {
             if (mainCamera.transform.position.y <= -11)
-            {
                 transform.SetParent(mainCamera.transform);
-            }
         }).OnComplete(delegate
         {
             coll.enabled = true;
             cameraTween = mainCamera.transform.DOMoveY(0, time * 5, false).OnUpdate(delegate
             {
                 if (mainCamera.transform.position.y >= -25f)
-                {
                     StopFishing();
-                }
             });
         });
+
+        ScreensManager.instance.ChangeScreen(Screens.GAME);
         coll.enabled = false;
         canMove = true;
         hookedFishes.Clear();
+
+
     }
+
     void StopFishing()
     {
         canMove = false;
@@ -79,41 +78,43 @@ public class Hook : MonoBehaviour
             {
                 transform.SetParent(null);
                 transform.position = new Vector2(transform.position.x, -6);
-
             }
         }).OnComplete(delegate
         {
             transform.position = Vector2.down * 6;
             coll.enabled = true;
-            int num = 0; 
+            int num = 0;
             for (int i = 0; i < hookedFishes.Count; i++)
             {
                 hookedFishes[i].transform.SetParent(null);
                 hookedFishes[i].ResetFish();
                 num += hookedFishes[i].Type.price;
             }
+            IdleManager.instance.totalGain = num;
+            ScreensManager.instance.ChangeScreen(Screens.END);
         });
-
     }
 
     private void OnTriggerEnter2D(Collider2D target)
     {
-        if(target.CompareTag("Fish") && fishCount!=strength)
+        if (target.CompareTag("Fish") && fishCount != strength)
         {
             fishCount++;
             Fish component = target.GetComponent<Fish>();
             component.Hooked();
             hookedFishes.Add(component);
             target.transform.SetParent(transform);
-            target.transform.position=hookedTransform.position;
-            target.transform.rotation = hookedTransform.rotation;
+            target.transform.position = hookedTransfrom.position;
+            target.transform.rotation = hookedTransfrom.rotation;
             target.transform.localScale = Vector3.one;
+
             target.transform.DOShakeRotation(5, Vector3.forward * 45, 10, 90, false).SetLoops(1, LoopType.Yoyo).OnComplete(delegate
             {
                 target.transform.rotation = Quaternion.identity;
-
             });
-            if(fishCount==strength) { StopFishing(); }
+            if (fishCount == strength)
+                StopFishing();
         }
     }
+
 }
